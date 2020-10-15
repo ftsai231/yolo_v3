c36c470b620930c5676af66fa6c5b62ca9e8a323
ftsai231 <f84024048@gamil.com>
2020-10-14 23:48:24 -0700

    added two more files

--- freeze_graph.py
+++ freeze_graph.py
@@ -1,19 +1,27 @@
 import tensorflow as tf
-from Yolo import YOLOV3
+from Yolo import YoloV3
+from config import cfg
+import numpy as np
 
+
+np.set_printoptions(threshold=np.inf)
 pb_file = "./yolov3_coco.pb"
-ckpt_file = "./checkpoint/yolov3_test_loss=6.8065.ckpt-140"
-output_node_names = ["input/input_data", "pred_sbbox/concat_2", "pred_mbbox/concat_2", "pred_lbbox/concat_2"]
+ckpt_file = "./checkpoint/train_checkpoint/yolov3_test_loss=14.3637.ckpt-700"
+output_node_names = ["input/input_data", "yolo_v3_model/pred_bbox/concat"]
 
 with tf.name_scope('input'):
-    input_data = tf.placeholder(dtype=tf.float32, name='input_data')
+    input_data = tf.placeholder(dtype=tf.float32, shape=[cfg.TRAIN.BATCH_SIZE, 416,
+                                                    416, 3], name='input_data')
 
-model = YOLOV3(input_data, trainable=False)
-print(model.conv_sbbox, model.conv_mbbox, model.conv_lbbox)
+model = YoloV3(input_data, training=False)
 
-sess  = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 saver = tf.train.Saver()
 saver.restore(sess, ckpt_file)
+
+'''print all the nodes in the graph. DO NOT DELETE'''
+# for v in sess.graph.get_operations():
+#                 print(v.name)
 
 converted_graph_def = tf.graph_util.convert_variables_to_constants(sess,
                             input_graph_def  = sess.graph.as_graph_def(),
